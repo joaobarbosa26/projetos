612580acfcb749808a5e99591c648757a6d473d2
joaobarbosa26 <jfvbarbosa98@gmail.com>
2023-12-10 01:03:24 +0000

    Changes to be committed: 	new file:   java.js 	modified:   portfolio.html 	modified:   style.css

---
+++ java.js
@@ -0,0 +1,11 @@
+// Add/Update this in your script.js
+function showOptions(card) {
+    const options = card.querySelector('.options');
+    options.style.display = 'block';
+  }
+  
+  function hideOptions(card) {
+    const options = card.querySelector('.options');
+    options.style.display = 'none';
+  }
+  
